e6af6358a39b4800c00f13ffebb33d1dd170283d
sud0nick <livingxsacrifice@gmail.com>
2018-01-12 21:47:31 -0500

    Version 1.4

--- CursedScreech/js/module.js
+++ CursedScreech/js/module.js
@@ -684,12 +684,17 @@ registerController('CursedScreechController', ['$api', '$scope', '$sce', '$inter
 			transformRequest: angular.identity,
 			headers: {'Content-Type': undefined}
 		}).then(function(response) {
-			for (var key in response) {
-				if (response.hasOwnProperty(key)) {
-					if (response.key == "Failed") {
-						alert("Failed to upload " + key);
+			var errors = {};
+			for (var key in response.data) {
+				if (response.data[key].success == "Failed") {
+					var msg = response.data[key].message + '\n';
+					if (!errors.hasOwnProperty(msg)) {
+						errors[msg] = true;
 					}
 				}
+			}
+			if (Object.keys(errors).length > 0) {
+				alert(Object.keys(errors).join(''));
 			}
 			$scope.selectedFiles = [];
 			$scope.getPayloads();
