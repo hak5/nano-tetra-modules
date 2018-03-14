5a632efb3672e1d60e9d5f6856b9fd33e8b7fd0f
Hacksawfred3232 <frederick.simmonds@gmail.com>
2018-03-14 13:31:38 +0000

    Added a "Test for log directory" function.
    Added a simple command that will test for the log directory. If the log directory is not there, the "|| " will catch the error and then run a one line function to print a warning to the connections log and then create the directory. This is a modified file for the SSLsplit module.

--- SSLsplit/scripts/sslsplit.sh
+++ SSLsplit/scripts/sslsplit.sh
@@ -22,7 +22,7 @@ if [ "$1" = "start" ]; then
 	sh /pineapple/modules/SSLsplit/rules/iptables
 
 	iptables -t nat -A POSTROUTING -j MASQUERADE
-
+	echo "If this file exists, it means that the script succesfully detected the log directory! > /pineapple/modules/SSLsplit/log/islogthere || printf "WRN: Log directory at '/pineapple/modules/SSLsplit/' was not found! We are creating it now!\n" > /pineapple/modules/SSLsplit/connections.log && mkdir /pineapple/modules/SSLsplit/log/ && echo "Directory succesfully created!" > /pineapple/modules/SSLsplit/log/islogthere
 	sslsplit -D -l /pineapple/modules/SSLsplit/connections.log -L /pineapple/modules/SSLsplit/log/output_${MYTIME}.log -k /pineapple/modules/SSLsplit/cert/certificate.key -c /pineapple/modules/SSLsplit/cert/certificate.crt ssl 0.0.0.0 8443 tcp 0.0.0.0 8080
 
 elif [ "$1" = "stop" ]; then
