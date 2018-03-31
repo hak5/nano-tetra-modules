c09828bf45d99a6aff271f69dd4e1d7bfcb4f235
catatonic <catatonicprime>
2018-03-31 21:30:28 +0000

    Including default torrc. This is preparation for building hiddenservices and the like.

--- tor/scripts/dependencies.sh
+++ tor/scripts/dependencies.sh
@@ -2,20 +2,21 @@
 # author: catatonicprime
 # date: March 2018
 
-pkg=tor
-
-touch /tmp/$pkg.progress
+touch /tmp/tor.progress
 
 if [ "$1" = "install" ]; then
   opkg update
   if [ "$2" = "internal" ]; then
-    opkg install $pkg
+    opkg install tor
   elif [ "$2" = "sd" ]; then
-    opkg install $pkg --dest sd
+    opkg install tor --dest sd
   fi
+  mkdir -p /etc/config/tor/
+  cp /pineapple/modules/tor/files/torrc /etc/config/tor
 elif [ "$1" = "remove" ]; then
-    opkg remove $pkg
+    opkg remove tor
 	sed -i '/tor\/scripts\/autostart_tor.sh/d' /etc/rc.local
+	rm -rf /etc/config/tor
 fi
 
-rm /tmp/$pkg.progress
+rm /tmp/tor.progress
