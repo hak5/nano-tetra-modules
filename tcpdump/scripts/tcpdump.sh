26002da4b3c3561deb5bbb17b4ae0c4f2c709cde
theblindrat <gmfpanda@gmail.com>
2018-08-19 11:46:54 -0400

    fixed default install script for tcpdump

--- tcpdump/scripts/tcpdump.sh
+++ tcpdump/scripts/tcpdump.sh
@@ -5,7 +5,16 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/sd/lib:/sd/usr/lib
 export PATH=$PATH:/sd/usr/bin:/sd/usr/sbin
 
 MYTIME=`date +%s`
-MYCMD=`cat /tmp/tcpdump.run`
+MYCMD="tcpdump `cat /tmp/tcpdump.run`"
+DUMPPATH='/pineapple/modules/tcpdump/dump/'
+
+echo "Starting if"
+if [ ! -d "$DUMPPATH" ]; then
+  # Control will enter here if $DUMPPATH doesn't exist.
+  # Recursively make the path
+  mkdir -p "$DUMPPATH"
+fi
+
 
 if [ "$1" = "start" ]; then
 	eval ${MYCMD}
