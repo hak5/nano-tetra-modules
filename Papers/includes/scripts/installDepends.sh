20469a73cbbacde4f10290d647884ef608c317c6
Andreas Nilsen <adde88@gmail.com>
2019-10-18 14:33:47 +0000

    Fixed dependencies scripts
    Signed-off-by: Andreas Nilsen <adde88@gmail.com>

--- Papers/includes/scripts/installDepends.sh
+++ Papers/includes/scripts/installDepends.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-#  Author: sud0nick
-#  Date:   Jan 2016
+#  Author: sud0nick & adde88
+#  Date:   18.10.2019
 
 opkg update > /dev/null;
-opkg install zip unzip > /dev/null;
+/etc/init.d/nginx stop > /dev/null;
+opkg remove nginx > /dev/null;
+opkg install zip unzip nginx-ssl > /dev/null;
+/etc/init.d/nginx restart > /dev/null;
 echo "Complete"
