7ea0c30f98ceba954e4d9297996fe1c69f5549b5
Andreas Nilsen <adde88@gmail.com>
2019-10-25 04:11:54 +0200

    Papers: Fixed dependencies scripts for latest fw 2.6.x (#83)
    * Fixed dependencies scripts
    
    Signed-off-by: Andreas Nilsen <adde88@gmail.com>
    
    * Bump to v1.8
    
    Signed-off-by: Andreas Nilsen <adde88@gmail.com>

--- Papers/includes/scripts/removeDepends.sh
+++ Papers/includes/scripts/removeDepends.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-#  Author: sud0nick
-#  Date:   Jan 2016
+#  Author: sud0nick & adde88
+#  Date:   18.10.2019
 
-opkg remove zip unzip > /dev/null;
+/etc/init.d/nginx stop > /dev/null;
+opkg remove zip unzip nginx-ssl > /dev/null;
+opkg install nginx > /dev/null;
